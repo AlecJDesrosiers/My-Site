aada66a013db952693de348005871b5023aec72b
Battleswab <98350243+Battleswab@users.noreply.github.com>
2023-02-09 22:23:21 -0500

    Projects section update

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,11 +1,60 @@
-
 export const Projects = () => {
-    const projects = [
+  const projects = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl,
+    },
+  ];
 
-    ]
-    return (
-        
-    )
-}
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col>
+            <h2>Projects</h2>
+            <p>Blah Blah Blah</p>
+            <Tab.Container id="projects-tabs" defaultActiveKey="first">
+              <Nav variant="pills" defaultActiveKey="/home">
+                <Nav.Item>
+                  <Nav.Link eventKey="first tab">Active</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second tab">Option 2</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third tab"> Tab 3 </Nav.Link>
+                </Nav.Item>
+              </Nav>
+              <Tab.Content>
+                <Tab.Pane></Tab.Pane>
+              </Tab.Content>
+            </Tab.Container>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
 
 export default Projects;
